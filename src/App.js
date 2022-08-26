2367d2f619fea2dfc7a5d26732b772dbb0a868b9
pnevmat <pnevmat001@gmail.com>
2022-08-26 13:21:09 +0300

    adds responsive styles

--- src/App.js
+++ src/App.js
@@ -4,31 +4,33 @@ import starWars from './images/star-wars-header-img.jpg';
 const App = () => {
   return (
     <div className="bg-gray-50 p-0">
-      <div className="h-64">
-        <img
-          src={starWars}
-          alt="Star Wars"
-          className="w-full h-full object-cover"
-        />
-      </div>
-      <div className="text-center pt-8 px-4">
-        <h1 className="text-4xl text-gray-900 font-extrabold tracking-tight">
-          Watch your favorite
-          <span className="block text-blue-700">movies and TV shows</span>
-        </h1>
-      </div>
-      <p className="text-lg text-gray-500 mt-3 max-w-md mx-auto">
-        Stream unlimited movies and TV shows on your phone, tablet, laptop and
-        TV.
-      </p>
-      <div className="mt-10">
-        <a
-          href="#"
-          className="bg-blue-700 px-5 py-3 rounded-lg shadow-lg font-semibold text-sm text-white inline-block w-full text-center"
-        >
-          Get Started!
-        </a>
-      </div>
+      <main className="lg:relative lg:max-w-7xl lg:mx-auto">
+        <div className="h-64 sm:h-72 md:h-96 lg:w-1/2 lg:absolute lg:right-0 lg:h-full">
+          <img
+            src={starWars}
+            alt="Star Wars"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="text-center pt-8 px-4 lg:w-1/2 lg:py-48 lg:text-left lg:max-w-lg">
+          <h1 className="text-4xl text-gray-900 font-extrabold tracking-tight sm:text-5-xl md:text-6xl">
+            Watch your favorite
+            <span className="block text-blue-700">movies and TV shows</span>
+          </h1>
+          <p className="text-lg text-gray-500 mt-3 max-w-md mx-auto sm:text-xl md:mt-5 md:max-w-3xl">
+            Stream unlimited movies and TV shows on your phone, tablet, laptop
+            and TV.
+          </p>
+          <div className="mt-10">
+            <a
+              href="#"
+              className="bg-blue-700 px-5 py-3 rounded-lg shadow-lg font-semibold text-sm text-white inline-block w-full text-center hover:text-white hover:bg-blue-800 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-blue-800 focus:ring-opacity-50 active:bg-blue-900 sm:w-auto md:px-10 md:py-4 md:text-lg"
+            >
+              Get Started!
+            </a>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
